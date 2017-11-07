2c823d17f992938f257552125495274472767971
Żaneta Jażdżyk <jazdzyk.zaneta@gmail.com>
2017-11-07 15:14:16 +0100

    todoService added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,26 @@
 import { Component } from '@angular/core';
 
 import {Todo} from './shared/todo.model';
+import {TodoService} from './shared/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoService]
 })
 export class AppComponent {
   title = 'Todo sample app';
+
+  constructor(private todoService: TodoService) {}
   lists = {
     todo: {
       name: 'To do',
-      tasks: [
-        new Todo('Todo #11'),
-        new Todo('Todo #12'),
-        new Todo('Todo #13'),
-        new Todo('Todo #14')
-      ]
+      tasks: this.todoService.getTodos()
     },
     done: {
       name: 'Done',
-      tasks: [
-        new Todo('Todo #15', true),
-        new Todo('Todo #16', true)
-      ]
+      tasks: this.todoService.getDoneTasks()
     }
   }
 
---
+++ src/app/shared/todo.service.ts
@@ -0,0 +1,24 @@
+import { Todo } from './todo.model';
+
+export class TodoService {
+  private tasks: Todo[] = [
+        new Todo('Todo #11'),
+        new Todo('Todo #12'),
+        new Todo('Todo #13'),
+        new Todo('Todo #14'),
+        new Todo('Todo #15', true),
+        new Todo('Todo #16', true)
+  ];
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  getTodos() {
+    return this.tasks.filter(task => !task.done);
+  }
+
+  getDoneTasks() {
+    return this.tasks.filter(task => task.done);
+  }
+}
