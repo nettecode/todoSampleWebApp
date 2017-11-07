3cfcb9dc705c5f44ecf0670a662743d0608931e4
Żaneta Jażdżyk <jazdzyk.zaneta@gmail.com>
2017-11-07 13:43:40 +0100

    todoList added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Todo sample app';
+  lists = {
+    todo: {
+      name: 'To do'
+    }
+  }
+
 }
